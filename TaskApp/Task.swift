f15ff887d2cf9ecc7c76c1bce70acad437b1f8c3
comu2e <dvlpEiji@gmail.com>
2016-06-27 01:17:15 +0900

    UI実装

---
+++ TaskApp/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  TaskApp
+//
+//  Created by Eiji Takahashi on 2016/06/27.
+//  Copyright © 2016年 devlpEiji. All rights reserved.
+//
+
+import Foundation
--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    //セルを選択した時に実行されるメソッド
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.Delete
+    }
+    
+    // Delete ボタンが押された時に呼ばれるメソッド
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
 }
 
