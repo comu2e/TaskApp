61f0e69fbe9e371113eca64ca992fe85f41e073e
comu2e <dvlpEiji@gmail.com>
2016-06-30 01:01:22 +0900

    通知実装

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, "9.0"
+use_frameworks!
+
+target 'TaskApp' do
+pod 'RealmSwift', '1.0.1'
+end
--- TaskApp/Task.swift
+++ TaskApp/Task.swift
@@ -1,9 +1,22 @@
-//
-//  Task.swift
-//  TaskApp
-//
-//  Created by Eiji Takahashi on 2016/06/27.
-//  Copyright © 2016年 devlpEiji. All rights reserved.
-//
+import RealmSwift
 
-import Foundation
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
